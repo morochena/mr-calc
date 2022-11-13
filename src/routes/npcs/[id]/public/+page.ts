14461b4fe0f0bb9f607bbeb872731888df2a60b1
Marcus Orochena <marcus.orochena@gmail.com>
2022-11-13 13:04:50 -0500

    fix preview

--- src/routes/npcs/[id]/public/+page.ts
+++ src/routes/npcs/[id]/public/+page.ts
@@ -10,6 +10,23 @@ export const load: PageLoad = async (event) => {
   }
 
   const { data: monster } = await supabaseClient.from('monsters').select('*').eq('id', event.params.id).single();
+
+  if (monster.spell_ids?.length > 0) {
+    const { data: spellData } = await supabaseClient.from('spells').select('*').in('id', monster.spell_ids);
+    monster.spells = spellData
+  }
+
+  if (!monster.spells) { monster.spells = [] }
+  if (!monster.spell_ids) { monster.spell_ids = [] }
+
+  if (monster.equipment_ids?.length > 0) {
+    const { data: equipmentData } = await supabaseClient.from('equipment').select('*').in('id', monster.equipment_ids);
+    monster.equipment = equipmentData
+  }
+
+  if (!monster.equipment) { monster.equipment = [] }
+  if (!monster.equipment_ids) { monster.equipment_ids = [] }
+
   const disableInputs = monster.user_id == session.user.id ? false : true;
 
   return {
