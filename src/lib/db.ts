1d5528cbb5d72d2e342af5dacdcec992bfbfae22
Marcus Orochena <marcus.orochena@gmail.com>
2022-11-13 12:21:24 -0500

    cloudflare build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient("https://ztpbyaqtzgefhmibdale.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0cGJ5YXF0emdlZmhtaWJkYWxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyMzM0MDEsImV4cCI6MTk4MTgwOTQwMX0.d4IE5zFB5cXF1x662OyLeeAuEcVf6oV9AoR4eiN0JjU");
