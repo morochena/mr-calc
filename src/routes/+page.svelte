e06cbcf1981e8c9de56daa7a04326a57f8c68821
Marcus Orochena <marcus.orochena@gmail.com>
2022-11-21 19:09:39 -0500

    update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,18 @@
 	};
 </script>
 
-<h1 class="text-3xl dark:text-white pb-8">Welcome to Mortal Reins Calculators</h1>
+<div class="p-16 pt-24 text-white">
+	<h1 class="text-3xl dark:text-white pb-8">Welcome to Mortal Reins Calculators</h1>
 
-<div class="row flex">
-	{#if !$page.data.session}
-		<Auth />
-	{:else}
-		<div>
-			<h1>You are logged in as: {$page.data.session.user.email}</h1>
+	<div class="row flex">
+		{#if !$page.data.session}
+			<Auth />
+		{:else}
+			<div>
+				<h1>You are logged in as: {$page.data.session.user.email}</h1>
 
-			<Button on:click={signOut} class="mt-4" color="red">Logout</Button>
-		</div>
-	{/if}
+				<Button on:click={signOut} class="mt-4" color="red">Logout</Button>
+			</div>
+		{/if}
+	</div>
 </div>
