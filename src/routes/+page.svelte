2edf6e1d77b985eb5849e83951a5e5c3a16daef3
Marcus Orochena <marcus.orochena@gmail.com>
2023-02-23 12:50:12 +0700

    add cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { supabaseClient } from '$lib/db';
-	import { Button } from 'flowbite-svelte';
+	import { Button, Card } from 'flowbite-svelte';
 	import Auth from '$lib/components/Auth.svelte';
 	import logo from '$lib/assets/logo.png';
 
@@ -11,25 +11,45 @@
 </script>
 
 <div class="p-16 pt-0 dark:text-white text-center flex items-center flex-col">
-	<img src={logo} width="500" alt="Mortal Reins" />
+	<img src={logo} width="300" alt="Mortal Reins" />
 
 	<div class="row flex">
 		{#if !$page.data.session}
 			<Auth />
 		{:else}
 			<div>
-				<h1>You are logged in as: {$page.data.session.user.email}</h1>
-				<Button on:click={signOut} class="mt-4" color="primary">Logout</Button>
+				<div class="row flex space-x-2 mb-4">
+					<Card href="/spells">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							Spells
+						</h5>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+							Create custom spells with specific effects and parameters to enhance your game
+							experience
+						</p>
+					</Card>
+					<Card href="/npcs">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							NPCs
+						</h5>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+							Generate combat statistics for your custom monsters and NPCs
+						</p>
+					</Card>
+					<Card href="/equipment">
+						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							Equipment
+						</h5>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+							Design equipment with tailored stats and features for your NPCs
+						</p>
+					</Card>
+				</div>
+				<div>
+					<h1>You are logged in as: {$page.data.session.user.email}</h1>
+					<Button on:click={signOut} class="mt-4" color="primary">Logout</Button>
+				</div>
 			</div>
 		{/if}
 	</div>
-
-	<h2 class="text-2xl mt-10">Changelog</h2>
-	<pre class="text-left">
-		## 2023-02-21
-		- layout refactor
-		- fix spell calculator bugs
-		- added profiles
-		- allowed public/private npcs/spells/equipment
-	</pre>
 </div>
