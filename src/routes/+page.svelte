d683cff35e70094ee0e2c9f2e1845b90713fe0b7
Marcus Orochena <marcus.orochena@gmail.com>
2023-03-15 10:25:00 -0400

    style(+layout.svelte): add background color and padding to main element style(+layout.svelte): change footer link color and hover color to match theme
    refactor(+page.svelte): improve layout and readability of the code
    
    refactor(tailwind.config.cjs): remove gray color palette from the configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,48 +13,46 @@
 	};
 </script>
 
-<div class="p-16 pt-0 dark:text-white text-center flex items-center flex-col">
-	<img src={logo} width="300" alt="Mortal Reins" />
+<div
+	class="min-h-screen bg-gray-100 dark:bg-gray-900 dark:text-white flex flex-col items-center justify-center p-4"
+>
+	<img src={logo} width="300" alt="Mortal Reins" class="mb-10" />
 
-	<div class="row flex">
-		{#if !$page.data.session}
-			<Auth />
-		{:else}
-			<div>
-				<div class="row flex space-x-2 mb-4">
-					<Card href="/spells">
-						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							Spells
-						</h5>
-						<img class="rounded-md" src={spells} width="500" alt="Spells" />
-						<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-							Create custom spells for your characters and NPCs
-						</p>
-					</Card>
-					<Card href="/npcs">
-						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							NPCs
-						</h5>
-						<img class="rounded-md" src={npcs} width="500" alt="NPCs" />
-						<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-							Generate combat statistics for your custom monsters and NPCs
-						</p>
-					</Card>
-					<Card href="/equipment">
-						<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							Equipment
-						</h5>
-						<img class="rounded-md" src={equipment} width="500" alt="Equipment" />
-						<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-							Design equipment with tailored stats and features for your NPCs
-						</p>
-					</Card>
-				</div>
-				<div>
-					<h1>You are logged in as: {$page.data.session.user.email}</h1>
-					<Button on:click={signOut} class="mt-4" color="primary">Logout</Button>
-				</div>
+	{#if !$page.data.session}
+		<Auth />
+	{:else}
+		<div class="w-full max-w-4xl">
+			<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+				<Card href="/spells" class="bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Spells
+					</h5>
+					<img class="rounded-md" src={spells} width="500" alt="Spells" />
+					<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+						Create custom spells for your characters and NPCs
+					</p>
+				</Card>
+				<Card href="/npcs" class="bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NPCs</h5>
+					<img class="rounded-md" src={npcs} width="500" alt="NPCs" />
+					<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+						Generate combat statistics for your custom monsters and NPCs
+					</p>
+				</Card>
+				<Card href="/equipment" class="bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Equipment
+					</h5>
+					<img class="rounded-md" src={equipment} width="500" alt="Equipment" />
+					<p class="mt-4 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+						Design equipment with tailored stats and features for your NPCs
+					</p>
+				</Card>
 			</div>
-		{/if}
-	</div>
+			<div class="text-center">
+				<h1 class="text-xl mb-4">You are logged in as: {$page.data.session.user.email}</h1>
+				<Button on:click={signOut} class="mt-4" color="primary">Logout</Button>
+			</div>
+		</div>
+	{/if}
 </div>
