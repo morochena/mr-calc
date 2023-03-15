90708cfa415719c55885810dc91b3c518afba201
Marcus Orochena <marcus.orochena@gmail.com>
2023-03-15 09:54:48 -0400

    refactor(Meta.svelte): dynamically generate domains array based on availableEffects data

--- src/routes/(spells)/spells/[id]/Meta.svelte
+++ src/routes/(spells)/spells/[id]/Meta.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
+	import { availableEffects } from '$lib/utils/data/effects';
 	import { Checkbox, Input, Label, Select, Textarea } from 'flowbite-svelte';
 	import type { Spell } from '../../../../../types/types';
 
 	export let spell: Spell;
 	export let disableInputs: boolean;
 
-	const domains = [
-		{ value: `Sorcery`, name: `Sorcery` },
-		{ value: `Fire`, name: `Fire` },
-		{ value: `Water`, name: `Water` },
-		{ value: `Earth`, name: `Earth` },
-		{ value: `Air`, name: `Air` },
-		{ value: `Necromancy`, name: `Necromancy` },
-		{ value: `Holy`, name: `Holy` },
-		{ value: `Mind`, name: `Mind` },
-		{ value: `Illusion`, name: `Illusion` },
-		{ value: `Nature`, name: `Nature` }
-	];
+	const domains = Array.from(new Set(availableEffects.flatMap((effect) => effect.domains))).map(
+		(domain) => ({ value: domain, name: domain })
+	);
+
+	// const domains = [
+	// 	{ value: `Sorcery`, name: `Sorcery` },
+	// 	{ value: `Fire`, name: `Fire` },
+	// 	{ value: `Water`, name: `Water` },
+	// 	{ value: `Earth`, name: `Earth` },
+	// 	{ value: `Air`, name: `Air` },
+	// 	{ value: `Necromancy`, name: `Necromancy` },
+	// 	{ value: `Holy`, name: `Holy` },
+	// 	{ value: `Mind`, name: `Mind` },
+	// 	{ value: `Illusion`, name: `Illusion` },
+	// 	{ value: `Nature`, name: `Nature` }
+	// ];
 
 	const modes = [
 		{ value: `Spell`, name: `Spell` },
