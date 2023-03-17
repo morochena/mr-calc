1f4caafe9fdd3845bf5f50e05943dd6183750d9e
Marcus Orochena <marcus.orochena@gmail.com>
2023-03-17 14:10:59 -0400

    refactor(spells/[id]/Effects.svelte): remove unused tryRemoveEffect function and commented out code
    refactor(Modifiers.svelte): remove tryRemoveModifier function and commented out code block

--- src/routes/(spells)/spells/[id]/Effects.svelte
+++ src/routes/(spells)/spells/[id]/Effects.svelte
@@ -84,11 +84,6 @@
 		addEffect(selectedEffect);
 		selectedEffect = null;
 	};
-
-	const tryRemoveEffect = (effect: Effect) => {
-		if (disableInputs) return;
-		removeEffect(effect);
-	};
 </script>
 
 <div class="bg-white dark:bg-gray-800 shadow-md rounded-md p-4 md:p-6 mb-6">
@@ -101,45 +96,3 @@
 		>Add Effect</Button
 	>
 </div>
-
-<!-- <Table class="mt-2">
-	<TableHead>
-		<TableHeadCell>Name</TableHeadCell>
-		<TableHeadCell>Tier</TableHeadCell>
-		<TableHeadCell>Notes</TableHeadCell>
-		<TableHeadCell>Description</TableHeadCell>
-	</TableHead>
-	<TableBody>
-		{#each spell.spell_data.effects as effect}
-			<TableBodyRow>
-				<TableBodyCell>{effect.name}</TableBodyCell>
-				<TableBodyCell>
-					{#if effect.hasTiers}
-						<Select
-							disabled={disableInputs}
-							class="w-20"
-							items={range(1, effect.maxTier + 1 || 50, 1).map((e) => ({ name: e, value: e }))}
-							bind:value={effect.tier}
-						/>
-					{/if}
-				</TableBodyCell>
-				<TableBodyCell
-					><Textarea
-						class="w-64"
-						rows="1"
-						bind:value={effect.notes}
-						disabled={disableInputs}
-					/></TableBodyCell
-				>
-				<TableBodyCell tdClass="px-6 py-4 font-medium flex justify-end"
-					><p>{calculateMOEDescription(spell, effect)}</p>
-					{#if !disableInputs}
-						<button on:click={() => tryRemoveEffect(effect)} class="ml-4"
-							><XCircle size="20" /></button
-						>
-					{/if}
-				</TableBodyCell>
-			</TableBodyRow>
-		{/each}
-	</TableBody>
-</Table> -->
--- src/routes/(spells)/spells/[id]/Modifiers.svelte
+++ src/routes/(spells)/spells/[id]/Modifiers.svelte
@@ -79,11 +79,6 @@
 		addModifier(selectedModifier);
 		selectedModifier = null;
 	};
-
-	const tryRemoveModifier = (modifier: Modifier) => {
-		if (disableInputs) return;
-		removeModifier(modifier);
-	};
 </script>
 
 <div class="bg-white dark:bg-gray-800 shadow-md rounded-md p-4 md:p-6 mb-6">
@@ -96,45 +91,3 @@
 		>Add Modifier</Button
 	>
 </div>
-
-<!-- <Table class="mt-2">
-	<TableHead>
-		<TableHeadCell>Name</TableHeadCell>
-		<TableHeadCell>Tier</TableHeadCell>
-		<TableHeadCell>Notes</TableHeadCell>
-		<TableHeadCell>Description</TableHeadCell>
-	</TableHead>
-	<TableBody>
-		{#each spell.spell_data.modifiers as modifier}
-			<TableBodyRow>
-				<TableBodyCell tdClass="px-6 py-4 font-medium">{modifier.name}</TableBodyCell>
-				<TableBodyCell>
-					{#if modifier.hasTiers}
-						<Select
-							disabled={disableInputs}
-							class="w-20"
-							items={range(1, modifier.maxTier + 1 || 50, 1).map((e) => ({ name: e, value: e }))}
-							bind:value={modifier.tier}
-						/>
-					{/if}
-				</TableBodyCell>
-				<TableBodyCell
-					><Textarea
-						class="w-64"
-						rows="1"
-						bind:value={modifier.notes}
-						disabled={disableInputs}
-					/></TableBodyCell
-				>
-				<TableBodyCell tdClass="px-6 py-4 font-medium flex justify-end"
-					><p>{calculateMOEDescription(spell, modifier)}</p>
-					{#if !disableInputs}
-						<button on:click={() => tryRemoveModifier(modifier)} class="ml-4"
-							><XCircle size="20" /></button
-						>
-					{/if}
-				</TableBodyCell>
-			</TableBodyRow>
-		{/each}
-	</TableBody>
-</Table> -->
