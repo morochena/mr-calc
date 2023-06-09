4b8efa8bdd4725ac49ad32bbbd9be1b4e2e0367a
Marcus Orochena <marcus.orochena@gmail.com>
2023-06-09 09:51:41 -0400

    mobile changes

--- src/routes/(spells)/spells/[id]/Calculation.svelte
+++ src/routes/(spells)/spells/[id]/Calculation.svelte
@@ -11,6 +11,10 @@
 		getProcessedModifiers
 	} from '$lib/utils/spells/getModifiersAndEffects';
 	import {
+		Card,
+		Dropdown,
+		DropdownItem,
+		MenuButton,
 		Select,
 		Table,
 		TableBody,
@@ -22,6 +26,7 @@
 	import type { Spell } from '../../../../../types/types';
 	import { Fire, Beaker, Sparkles, XMark } from 'svelte-heros';
 	import { calcNumberOfPowers } from '$lib/utils/spells/descriptionFunctions/calcNumberOfPowers';
+	import MediaQuery from '$lib/components/MediaQuery.svelte';
 
 	export let spell: Spell;
 	export let disableInputs: boolean;
@@ -100,112 +105,226 @@
 		</div>
 	</div>
 
-	<Table class="mt-2" divClass="relative">
-		<TableHead>
-			<TableHeadCell>Name</TableHeadCell>
-			<TableHeadCell>SP</TableHeadCell>
-			<TableHeadCell>Tier</TableHeadCell>
-			<TableHeadCell>Notes</TableHeadCell>
-			<TableHeadCell>Description</TableHeadCell>
-			<TableHeadCell />
-		</TableHead>
-		<TableBody>
+	<MediaQuery query="(max-width: 480px)" let:matches>
+		{#if matches}
 			{#each getProcessedModifiers(spell) as modifier}
-				<TableBodyRow>
-					<TableBodyCell>
-						<div class="flex items-center">
-							{#if !modifier.prerequisite || modifier.prerequisite.length <= 0}
-								<Sparkles size="16" class="mr-2" />
-							{/if}{modifier.name}
-						</div>
-					</TableBodyCell>
-					<TableBodyCell>{calcSPValueText(spell, modifier)}</TableBodyCell>
-					<TableBodyCell>
-						{#if modifier.hasTiers && !modifier.fromDomain}
-							<Select
-								disabled={disableInputs}
-								size="sm"
-								items={range(1, modifier.maxTier + 1 || 50, 1).map((e) => ({ name: e, value: e }))}
-								on:change={(e) => updateModifierTier(modifier.id, e.target.value)}
-								value={modifier.tier}
-							/>
-						{:else}
-							{modifier.tier}
-						{/if}
-					</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-4 font-medium">{modifier.notes || ''}</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-4 font-medium">
-						<FillInBlanks
-							moeId={modifier.id}
-							baseText={calculateMOEDescription(spell, modifier)}
-							bind:replacementValue={modifier.meta}
-							onChange={updateModifierMetaValue}
-							disabled={disableInputs}
-						/>
-					</TableBodyCell>
-					<TableBodyCell>
+				<Card class="mb-2">
+					<div class="flex justify-end">
 						{#if !disableInputs && !modifier.fromDomain}
-							<button on:click={() => removeModifier(modifier.id)}>
-								<XMark />
-							</button>
+							<MenuButton />
+							<Dropdown class="w-36">
+								<DropdownItem on:click={() => removeEffect(modifier.id)}>Delete</DropdownItem>
+							</Dropdown>
 						{/if}
-					</TableBodyCell>
-				</TableBodyRow>
-			{/each}
-			{#each getProcessedEffects(spell) as effect}
-				<TableBodyRow>
-					<TableBodyCell>
+					</div>
+					<h4 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
 						<div class="flex items-center">
-							{#if !effect.prerequisite || effect.prerequisite.length <= 0}
-								<Fire size="16" class="mr-2" />
-							{/if}{effect.name}
+							{modifier.name}
 						</div>
-					</TableBodyCell>
-					<TableBodyCell>{calcSPValueText(spell, effect)}</TableBodyCell>
-					<TableBodyCell>
-						{#if effect.hasTiers && !effect.fromDomain}
-							<Select
+					</h4>
+					<div class="text-sm text-gray-700 dark:text-gray-400">
+						<p class="mb-1">
+							<strong>SP Cost:</strong>
+							{calcSPValueText(spell, modifier)}
+						</p>
+						<p class="mb-1">
+							<strong>Tier:</strong>
+							{#if modifier.hasTiers && !modifier.fromDomain}
+								<Select
+									disabled={disableInputs}
+									size="sm"
+									items={range(1, modifier.maxTier + 1 || 50, 1).map((e) => ({
+										name: e,
+										value: e
+									}))}
+									on:change={(e) => updateModifierTier(modifier.id, e.target.value)}
+									value={modifier.tier}
+								/>
+							{:else}
+								{modifier.tier}
+							{/if}
+						</p>
+						<p class="mb-1">
+							<strong>Description:</strong>
+							<FillInBlanks
+								moeId={modifier.id}
+								baseText={calculateMOEDescription(spell, modifier)}
+								bind:replacementValue={modifier.meta}
+								onChange={updateModifierMetaValue}
 								disabled={disableInputs}
-								size="sm"
-								items={range(1, effect.maxTier + 1 || 50, 1).map((e) => ({ name: e, value: e }))}
-								on:change={(e) => updateEffectTier(effect.id, e.target.value)}
-								value={effect.tier}
 							/>
-						{:else}
-							{effect.tier}
-						{/if}
-					</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-4 font-medium">{effect.notes || ''}</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-4 font-medium">
-						<FillInBlanks
-							moeId={effect.id}
-							baseText={calculateMOEDescription(spell, effect)}
-							bind:replacementValue={effect.meta}
-							onChange={updateEffectMetaValue}
-							disabled={disableInputs}
-						/></TableBodyCell
-					>
-
-					<TableBodyCell>
-						{#if !disableInputs && !effect.fromDomain}
-							<button on:click={() => removeEffect(effect.id)}>
-								<XMark />
-							</button>
-						{/if}
-					</TableBodyCell>
-				</TableBodyRow>
-			{/each}
-			<TableBodyRow>
-				<TableBodyCell>Total</TableBodyCell>
-				<TableBodyCell
-					>{calculateTotalSP(spell).adds} x {calculateTotalSP(spell).mults.toFixed(2)} =
-					<strong>{calculateTotalSP(spell).cost}</strong></TableBodyCell
+						</p>
+					</div></Card
 				>
-				<TableBodyCell># Powers: {calcNumberOfPowers(spell)}</TableBodyCell>
-				<TableBodyCell />
-				<TableBodyCell />
-				<TableBodyCell />
-			</TableBodyRow>
-		</TableBody>
-	</Table>
+			{/each}
+
+			{#each getProcessedEffects(spell) as effect}
+				<Card class="mb-2">
+					<div class="flex justify-end">
+						{#if !disableInputs && !effect.fromDomain}
+							<MenuButton />
+							<Dropdown class="w-36">
+								<DropdownItem on:click={() => removeEffect(effect.id)}>Delete</DropdownItem>
+							</Dropdown>
+						{/if}
+					</div>
+					<h4 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+						<div class="flex items-center">
+							{effect.name}
+						</div>
+					</h4>
+					<div class="text-sm text-gray-700 dark:text-gray-400">
+						<p class="mb-1">
+							<strong>SP Cost:</strong>
+							{calcSPValueText(spell, effect)}
+						</p>
+
+						<p class="mb-1">
+							<strong>Tier:</strong>
+							{#if effect.hasTiers && !effect.fromDomain}
+								<Select
+									disabled={disableInputs}
+									size="sm"
+									items={range(1, effect.maxTier + 1 || 50, 1).map((e) => ({ name: e, value: e }))}
+									on:change={(e) => updateEffectTier(effect.id, e.target.value)}
+									value={effect.tier}
+								/>
+							{:else}
+								{effect.tier}
+							{/if}
+						</p>
+						<p class="mb-1">
+							<strong>Description:</strong>
+							<FillInBlanks
+								moeId={effect.id}
+								baseText={calculateMOEDescription(spell, effect)}
+								bind:replacementValue={effect.meta}
+								onChange={updateEffectMetaValue}
+								disabled={disableInputs}
+							/>
+						</p>
+					</div>
+				</Card>
+			{/each}
+		{/if}
+	</MediaQuery>
+
+	<MediaQuery query="(min-width: 481px)" let:matches>
+		{#if matches}
+			<Table class="mt-2" divClass="relative">
+				<TableHead>
+					<TableHeadCell>Name</TableHeadCell>
+					<TableHeadCell>SP</TableHeadCell>
+					<TableHeadCell>Tier</TableHeadCell>
+					<TableHeadCell>Notes</TableHeadCell>
+					<TableHeadCell>Description</TableHeadCell>
+					<TableHeadCell />
+				</TableHead>
+				<TableBody>
+					{#each getProcessedModifiers(spell) as modifier}
+						<TableBodyRow>
+							<TableBodyCell>
+								<div class="flex items-center">
+									{#if !modifier.prerequisite || modifier.prerequisite.length <= 0}
+										<Sparkles size="16" class="mr-2" />
+									{/if}{modifier.name}
+								</div>
+							</TableBodyCell>
+							<TableBodyCell>{calcSPValueText(spell, modifier)}</TableBodyCell>
+							<TableBodyCell>
+								{#if modifier.hasTiers && !modifier.fromDomain}
+									<Select
+										disabled={disableInputs}
+										size="sm"
+										items={range(1, modifier.maxTier + 1 || 50, 1).map((e) => ({
+											name: e,
+											value: e
+										}))}
+										on:change={(e) => updateModifierTier(modifier.id, e.target.value)}
+										value={modifier.tier}
+									/>
+								{:else}
+									{modifier.tier}
+								{/if}
+							</TableBodyCell>
+							<TableBodyCell tdClass="px-6 py-4 font-medium">{modifier.notes || ''}</TableBodyCell>
+							<TableBodyCell tdClass="px-6 py-4 font-medium">
+								<FillInBlanks
+									moeId={modifier.id}
+									baseText={calculateMOEDescription(spell, modifier)}
+									bind:replacementValue={modifier.meta}
+									onChange={updateModifierMetaValue}
+									disabled={disableInputs}
+								/>
+							</TableBodyCell>
+							<TableBodyCell>
+								{#if !disableInputs && !modifier.fromDomain}
+									<button on:click={() => removeModifier(modifier.id)}>
+										<XMark />
+									</button>
+								{/if}
+							</TableBodyCell>
+						</TableBodyRow>
+					{/each}
+					{#each getProcessedEffects(spell) as effect}
+						<TableBodyRow>
+							<TableBodyCell>
+								<div class="flex items-center">
+									{#if !effect.prerequisite || effect.prerequisite.length <= 0}
+										<Fire size="16" class="mr-2" />
+									{/if}{effect.name}
+								</div>
+							</TableBodyCell>
+							<TableBodyCell>{calcSPValueText(spell, effect)}</TableBodyCell>
+							<TableBodyCell>
+								{#if effect.hasTiers && !effect.fromDomain}
+									<Select
+										disabled={disableInputs}
+										size="sm"
+										items={range(1, effect.maxTier + 1 || 50, 1).map((e) => ({
+											name: e,
+											value: e
+										}))}
+										on:change={(e) => updateEffectTier(effect.id, e.target.value)}
+										value={effect.tier}
+									/>
+								{:else}
+									{effect.tier}
+								{/if}
+							</TableBodyCell>
+							<TableBodyCell tdClass="px-6 py-4 font-medium">{effect.notes || ''}</TableBodyCell>
+							<TableBodyCell tdClass="px-6 py-4 font-medium">
+								<FillInBlanks
+									moeId={effect.id}
+									baseText={calculateMOEDescription(spell, effect)}
+									bind:replacementValue={effect.meta}
+									onChange={updateEffectMetaValue}
+									disabled={disableInputs}
+								/></TableBodyCell
+							>
+
+							<TableBodyCell>
+								{#if !disableInputs && !effect.fromDomain}
+									<button on:click={() => removeEffect(effect.id)}>
+										<XMark />
+									</button>
+								{/if}
+							</TableBodyCell>
+						</TableBodyRow>
+					{/each}
+					<TableBodyRow>
+						<TableBodyCell>Total</TableBodyCell>
+						<TableBodyCell
+							>{calculateTotalSP(spell).adds} x {calculateTotalSP(spell).mults.toFixed(2)} =
+							<strong>{calculateTotalSP(spell).cost}</strong></TableBodyCell
+						>
+						<TableBodyCell># Powers: {calcNumberOfPowers(spell)}</TableBodyCell>
+						<TableBodyCell />
+						<TableBodyCell />
+						<TableBodyCell />
+					</TableBodyRow>
+				</TableBody>
+			</Table>
+		{/if}
+	</MediaQuery>
 </div>
